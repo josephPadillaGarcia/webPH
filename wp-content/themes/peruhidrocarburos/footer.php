e54a253254b45462cb40787a2fcd1e09b7cee106
josephpg <jlpadillagarcia19@gmail.com>
2024-05-08 00:40:27 -0500

    footer process

--- wp-content/themes/peruhidrocarburos/footer.php
+++ wp-content/themes/peruhidrocarburos/footer.php
@@ -1,5 +1,45 @@
-<?php wp_footer(); ?>
+<footer class="footer">
+    <div class="container">
+        <div class="grid-col">
+            <div class="grid-s-12 grid-m-6 grid-l-4">
 
+                <?php
+                    $args = array(
+                        'post_type' => 'informacion_general',
+                        'posts_per_page' => '0',
+                    );
+                ?>
+                <?php $the_query = new WP_Query($args); ?>
+                    <?php if ($the_query->have_posts()) : ?>
+                        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                            <div class="footer__logo">
+                                <?php $imagen = get_field('logo_ph_blanco'); ?>    
+                                <img src="<?php echo $imagen['url']; ?>" alt="" class="">
+                            </div>
+                            <div class="infocontactos">
+                                <?php the_field('informacion_de_contacto'); ?>
+                            </div>
+                            <div class="redes">
+                                <?php if (have_rows('item_redes')) { ?>
+                                    <?php while (have_rows('item_redes')) {
+                                    the_row(); ?>
+                                        <div class="itemredes">
+                                            <a href="<?php the_sub_field('link_redes'); ?>"><i class="ri-<?php the_sub_field('icon_redes'); ?>-fill"></i></a>
+                                        </div>
+                                    <?php } ?>
+                                <?php } ?>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
+
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<?php wp_footer(); ?>
 
 <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/fancy.min.css">
--- wp-content/themes/peruhidrocarburos/index.php
+++ wp-content/themes/peruhidrocarburos/index.php
@@ -201,7 +201,56 @@ get_header();
     </div>
 </section>
 
+<section class="section-blog">
+    <div class="container">
+        <div class="title-general title-general--left">
+            <?php the_field('titulo_blog'); ?>
+        </div>
 
+        <div class="contentblog">
+            <div class="grid-col">
+
+            <?php
+                $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => '0',
+                );
+            ?>
+            <?php $the_query = new WP_Query($args); ?>
+                <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                        
+                        <div class="grid-s-12 grid-m-6 grid-l-4">
+                            <div class="itemblog">
+                                <figure class="card__figure">
+                                    <?php $imagen = get_field('imagen_post'); ?>
+                                    <img class="card__image" src="<?php echo $imagen['url']; ?>">
+                                </figure>
+                                <div class="infoblog">
+                                    <div class="autorblog">
+                                        <i class="ri-user-fill"></i><p>por: <?php the_field('autor_post'); ?></p>
+                                    </div>
+                                    <div class="titleblog">
+                                        <h4><?php the_title(); ?></h4>
+                                    </div>
+                                    <div class="enlaceblog">
+                                        <a href="<?php the_permalink(); ?>">Leer Más</a>
+                                        <p><?php the_field('fecha_post'); ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+
+
+            </div>
+        </div>
+    </div>
+</section>
 
 
 <?php 
